373ddbe7c786b2bf15f878b5fdefc39e45255d52
MacAposAGH <mapostol@student.agh.edu.pl>
2026-05-17 12:01:05 +0200

    first commit

--- app/src/main/java/com/example/counter/MainActivity.java
+++ app/src/main/java/com/example/counter/MainActivity.java
@@ -1,6 +1,8 @@
 package com.example.counter;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,6 +11,8 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
+
+    private int counter = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,5 +24,26 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+    }
+
+    private void setViewText() {
+        String s = String.valueOf(counter);
+        TextView textView = findViewById(R.id.counter_value);
+        textView.setText(String.valueOf(counter));
+    }
+
+    public void increment(View view) {
+        counter++;
+        setViewText();
+    }
+
+    public void decrement(View view) {
+        counter--;
+        setViewText();
+    }
+
+    public void reset(View view) {
+        counter = 0;
+        setViewText();
     }
 }
